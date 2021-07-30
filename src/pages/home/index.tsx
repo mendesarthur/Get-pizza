ae0f334c78e4e2a0eb13ad18d734e72f5e9198b5
arthurmendes <84751128+mendesarthur@users.noreply.github.com>
2021-07-30 00:37:19 -0300

    rotas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,7 +5,8 @@ const Home = () => {
        <>
         <GlobalMenu /> 
         <h1>Home</h1>
-
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim accusamus totam natus temporibus dolor magni deleniti expedita, at, molestias non officiis debitis, fugit aspernatur quam vel culpa quibusdam mollitia. Quibusdam.</p>
+        
         </>
     );
 }
--- src/pages/tests/index.tsx
+++ src/pages/tests/index.tsx
@@ -12,54 +12,52 @@ const Tests = () => {
         <>
             <GlobalMenu />
             <h1>Tests</h1>
-            <h2>Modal Bootsrap
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
-            </button>
+            <h2>Modal Bootstrap
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Launch demo modal
+                </button>
             </h2>
 
             <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                    meu exemplo
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
-                </div>
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            Meu texto exemplo
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-            <h2>Modal React-Bootstrape</h2>
+            
+            <h2>Modal React-Bootstrap
+                <Button variant="primary" onClick={handleModalShow}>
+                    Launch demo modal
+                </Button>
+            </h2>
             <Modal
-            show={modalShow}
-            onHide={handleModalClose}
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
+                show={modalShow}
+                onHide={handleModalClose}
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
             >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                Modal heading
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <h4>Centered Modal</h4>
-                <p>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                consectetur ac, vestibulum at eros.
-                </p>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={handleModalClose}>Close</Button>
-            </Modal.Footer>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Modal title
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Meu texto exemplo
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleModalClose}>Close</Button>
+                </Modal.Footer>
             </Modal>
-        
         </>
     );
 }
